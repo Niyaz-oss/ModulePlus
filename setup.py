15489fbe33b7614033df1f309561c4537458d898
Niyaz-oss <m.niyazramadhan@gmail.com>
2026-08-20 17:53:41 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="moduleplus",
+    version="1.0.0",
+    description="A Python mega-module with useful modules and helper functions",
+    packages=find_packages(),
+    python_requires=">=3.8",
+)
